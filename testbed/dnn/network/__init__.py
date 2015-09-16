26441c3aa62129e06c10f956df7aef1f844a9f84
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2015-09-16 10:35:40 +1000

    refactoring: deleted unnecessary codes

--- testbed/dnn/network/__init__.py
+++ testbed/dnn/network/__init__.py
@@ -2,9 +2,5 @@ __author__ = 'masayuki'
 
 from dA import dA
 from SdA import SdA
-# from stacked_lstm import StackedLSTM
-# from stacked_conv_lstm import StackedConvLSTM
 from stacked_networks import StackedLSTM, StackedConvLSTM
-# from encoder_decoder_lstm import EncoderDecoderLSTM
-# from encoder_decoder_conv_lstm import EncoderDecoderConvLSTM
 from encoder_decoder_networks import EncoderDecoderLSTM, EncoderDecoderConvLSTM
