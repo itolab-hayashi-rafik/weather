635e130dd99bd5f57cb272c6576f7d773c9d85fb
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2015-09-18 15:45:46 +1000

    removed unnecessary codes

--- testbed/utils.py
+++ testbed/utils.py
@@ -5,12 +5,6 @@ import numpy
 import theano
 import theano.tensor as T
 
-from PySide import QtGui
-sys.modules['PyQt4.QtGui'] = QtGui
-
-from PIL import Image
-from PIL.ImageQt import ImageQt
-
 def ndarray(x):
     '''
     returns ndarray of x
@@ -31,24 +25,3 @@ def flattened(i):
                 yield b
         else:
             yield a
-
-def generateImage(data):
-    '''
-
-    :param data:
-    :return:
-    '''
-    assert isinstance(data, numpy.ndarray)
-
-    if len(data.shape) == 2:
-        return Image.fromarray(numpy.uint8(data*255))
-    elif len(data.shape) == 3:
-        return [ Image.fromarray(numpy.uint8(slice*255)) for slice in data ]
-    else:
-        raise TypeError("input data must be d-by-h-by-w ndarray")
-
-def PILimageToQImage(pilimage):
-    """converts a PIL image to QImage"""
-    imageq = ImageQt(pilimage)
-    qimage = QtGui.QImage(imageq)
-    return qimage
