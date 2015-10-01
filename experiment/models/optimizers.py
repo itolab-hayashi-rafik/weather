54bf843dfbfdf2cf5b5a01b0c84649c97e1799f8
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2015-10-01 20:06:55 +1000

    fixed RMSProp optimizer

--- experiment/models/optimizers.py
+++ experiment/models/optimizers.py
@@ -180,7 +180,7 @@ def my_rmsprop(lr, params, grads, x, mask, y, shared_x, shared_mask, shared_y, i
     # build update lists
     new_r_list = [decay_rate*r + (1-decay_rate)*tensor.square(g) for (g, r) in zip(grads, r_list)]
     upd_r_list = [(r, new_r) for (r, new_r) in zip(r_list, new_r_list)]
-    new_w_list = [w - (lr / (tensor.sqrt(new_r)+epsilon)) * g for (g, w, new_r) in zip(grads, w_list, new_r_list)]
+    new_w_list = [w - (lr / (tensor.sqrt(new_r)+epsilon)) * g for (g, w, new_r) in zip(grads, params, new_r_list)]
     upd_w_list = [(w, new_w) for (w, new_w) in zip (params, new_w_list)]
 
     # build a function to calculate r
