f2fd0c1bc4f65f5b50bd754eb867f12a61bd3978
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2015-10-03 14:34:49 +1000

    fixed LSTM and ConvLSTM to accept None as their inputs

--- testbed/dnn/network/layer/conv_lstm.py
+++ testbed/dnn/network/layer/conv_lstm.py
@@ -14,7 +14,7 @@ class ConvLSTM(RNN):
     see: http://deeplearning.net/tutorial/lstm.html
     see: https://github.com/JonathanRaiman/theano_lstm/blob/master/theano_lstm/__init__.py
     """
-    def __init__(self, input_shape, filter_shape, activation=T.tanh, clip_gradients=False, prefix="ConvLSTM", **kwargs):
+    def __init__(self, input_shape, filter_shape, has_input=True, activation=T.tanh, clip_gradients=False, prefix="ConvLSTM", **kwargs):
         '''
          initialize ConvLSTM
 
@@ -39,6 +39,7 @@ class ConvLSTM(RNN):
         self.input_filter_shape = filter_shape
         self.hidden_filter_shape = (filter_shape[0], filter_shape[0], filter_shape[2], filter_shape[3])
         self.output_shape = (filter_shape[0], input_shape[1], input_shape[2])
+        self.has_input = has_input
 
         # ConvLSTM receives in total:
         # "num of input feature maps * input height * input width" inputs
@@ -85,8 +86,9 @@ class ConvLSTM(RNN):
 
     def setup(self):
         # initialize weights
-        Wxf_value = self.random_initialization(self.input_filter_shape)
-        self.Wxf = self._shared(Wxf_value, name="Wxf", borrow=True)
+        if self.has_input:
+            Wxf_value = self.random_initialization(self.input_filter_shape)
+            self.Wxf = self._shared(Wxf_value, name="Wxf", borrow=True)
         Whf_value = self.random_initialization(self.hidden_filter_shape)
         self.Whf = self._shared(Whf_value, name="Whf", borrow=True)
         Wcf_value = self.random_initialization((self.output_shape[0],))
@@ -94,8 +96,9 @@ class ConvLSTM(RNN):
         bf_value = numpy.zeros((self.output_shape[0],), dtype=theano.config.floatX)
         self.bf = self._shared(bf_value, name="bf", borrow=True)
 
-        Wxi_value = self.random_initialization(self.input_filter_shape)
-        self.Wxi = self._shared(Wxi_value, name="Wxi", borrow=True)
+        if self.has_input:
+            Wxi_value = self.random_initialization(self.input_filter_shape)
+            self.Wxi = self._shared(Wxi_value, name="Wxi", borrow=True)
         Whi_value = self.random_initialization(self.hidden_filter_shape)
         self.Whi = self._shared(Whi_value, name="Whi", borrow=True)
         Wci_value = self.random_initialization((self.output_shape[0],))
@@ -103,15 +106,17 @@ class ConvLSTM(RNN):
         bi_value = numpy.zeros((self.output_shape[0],), dtype=theano.config.floatX)
         self.bi = self._shared(bi_value, name="bi", borrow=True)
 
-        Wxc_value = self.random_initialization(self.input_filter_shape)
-        self.Wxc = self._shared(Wxc_value, name="Wxc", borrow=True)
+        if self.has_input:
+            Wxc_value = self.random_initialization(self.input_filter_shape)
+            self.Wxc = self._shared(Wxc_value, name="Wxc", borrow=True)
         Whc_value = self.random_initialization(self.hidden_filter_shape)
         self.Whc = self._shared(Whc_value, name="Whc", borrow=True)
         bc_value = numpy.zeros((self.output_shape[0],), dtype=theano.config.floatX)
         self.bc = self._shared(bc_value, name="bc", borrow=True)
 
-        Wxo_value = self.random_initialization(self.input_filter_shape)
-        self.Wxo = self._shared(Wxo_value, name="Wxo", borrow=True)
+        if self.has_input:
+            Wxo_value = self.random_initialization(self.input_filter_shape)
+            self.Wxo = self._shared(Wxo_value, name="Wxo", borrow=True)
         Who_value = self.random_initialization(self.hidden_filter_shape)
         self.Who = self._shared(Who_value, name="Who", borrow=True)
         Wco_value = self.random_initialization((self.output_shape[0],))
@@ -119,20 +124,29 @@ class ConvLSTM(RNN):
         bo_value = numpy.zeros((self.output_shape[0],), dtype=theano.config.floatX)
         self.bo = self._shared(bo_value, name="bo", borrow=True)
 
-    def step(self, m_, x_, c_, h_):
-        # assume x_ is of shape (n_samples, num of input feature maps, input height, input width),
+    def step(self, m, x, c_, h_):
+        # assume x is of shape (n_samples, num of input feature maps, input height, input width),
         # c_ is of shape (n_samples, num of hidden feature maps, output height, output width),
         # h_ is of shape (n_samples, num of output feature maps, output height, output width).
         # Note num of hidden feature maps = num of output feature maps,
         # input height = output height, and input width = output width
 
-        f = T.nnet.sigmoid(self.conv_x(x_, self.Wxf) + self.conv_h(h_, self.Whf) + self.Wcf.dimshuffle('x',0,'x','x') * c_ + self.bf.dimshuffle('x',0,'x','x'))
-        i = T.nnet.sigmoid(self.conv_x(x_, self.Wxi) + self.conv_h(h_, self.Whi) + self.Wci.dimshuffle('x',0,'x','x') * c_ + self.bi.dimshuffle('x',0,'x','x'))
-        c = self.activation(self.conv_x(x_, self.Wxc) + self.conv_h(h_, self.Whc) + self.bc.dimshuffle('x',0,'x','x'))
-        c = f * c_ + i * c
+        if self.has_input:
+            f = T.nnet.sigmoid(self.conv_x(x, self.Wxf) + self.conv_h(h_, self.Whf) + c_ * self.Wcf.dimshuffle('x',0,'x','x') + self.bf.dimshuffle('x',0,'x','x'))
+            i = T.nnet.sigmoid(self.conv_x(x, self.Wxi) + self.conv_h(h_, self.Whi) + c_ * self.Wci.dimshuffle('x',0,'x','x') + self.bi.dimshuffle('x',0,'x','x'))
+            c = self.activation(self.conv_x(x, self.Wxc) + self.conv_h(h_, self.Whc) + self.bc.dimshuffle('x',0,'x','x'))
+            c = f * c_ + i * c
 
-        o = T.nnet.sigmoid(self.conv_x(x_, self.Wxo) + self.conv_h(h_, self.Who) + self.Wco.dimshuffle('x',0,'x','x') * c  + self.bo.dimshuffle('x',0,'x','x'))
-        h = o * self.activation(c)
+            o = T.nnet.sigmoid(self.conv_x(x, self.Wxo) + self.conv_h(h_, self.Who) + c  * self.Wco.dimshuffle('x',0,'x','x') + self.bo.dimshuffle('x',0,'x','x'))
+            h = o * self.activation(c)
+        else:
+            f = T.nnet.sigmoid(self.conv_h(h_, self.Whf) + c_ * self.Wcf.dimshuffle('x',0,'x','x') + self.bf.dimshuffle('x',0,'x','x'))
+            i = T.nnet.sigmoid(self.conv_h(h_, self.Whi) + c_ * self.Wci.dimshuffle('x',0,'x','x') + self.bi.dimshuffle('x',0,'x','x'))
+            c = self.activation(self.conv_h(h_, self.Whc) + self.bc.dimshuffle('x',0,'x','x'))
+            c = f * c_ + i * c
+
+            o = T.nnet.sigmoid(self.conv_h(h_, self.Who) + c  * self.Wco.dimshuffle('x',0,'x','x') + self.bo.dimshuffle('x',0,'x','x'))
+            h = o * self.activation(c)
 
         return c, h
 
@@ -145,26 +159,46 @@ class ConvLSTM(RNN):
 
     @property
     def params(self):
-        return [self.Wxf, self.Whf, self.Wcf, self.bf,
-                self.Wxi, self.Whi, self.Wci, self.bi,
-                self.Wxc, self.Whc, self.bc,
-                self.Wxo, self.Who, self.Wco, self.bo]
+        if self.has_input:
+            return [self.Wxf, self.Whf, self.Wcf, self.bf,
+                    self.Wxi, self.Whi, self.Wci, self.bi,
+                    self.Wxc, self.Whc, self.bc,
+                    self.Wxo, self.Who, self.Wco, self.bo]
+        else:
+            return [self.Whf, self.Wcf, self.bf,
+                    self.Whi, self.Wci, self.bi,
+                    self.Whc, self.bc,
+                    self.Who, self.Wco, self.bo]
 
     @params.setter
     def params(self, param_list):
-        self.Wxf.set_value(param_list[0].get_value())
-        self.Whf.set_value(param_list[1].get_value())
-        self.Wcf.set_value(param_list[2].get_value())
-        self.bf.set_value(param_list[3].get_value())
-        self.Wxi.set_value(param_list[4].get_value())
-        self.Whi.set_value(param_list[5].get_value())
-        self.Wci.set_value(param_list[6].get_value())
-        self.bi.set_value(param_list[7].get_value())
-        self.Wxc.set_value(param_list[8].get_value())
-        self.Whc.set_value(param_list[9].get_value())
-        self.bc.set_value(param_list[10].get_value())
-        self.Wxo.set_value(param_list[11].get_value())
-        self.Who.set_value(param_list[12].get_value())
-        self.Wco.set_value(param_list[13].get_value())
-        self.bo.set_value(param_list[14].get_value())
+        if self.has_input:
+            self.Wxf.set_value(param_list[0].get_value())
+            self.Whf.set_value(param_list[1].get_value())
+            self.Wcf.set_value(param_list[2].get_value())
+            self.bf.set_value(param_list[3].get_value())
+            self.Wxi.set_value(param_list[4].get_value())
+            self.Whi.set_value(param_list[5].get_value())
+            self.Wci.set_value(param_list[6].get_value())
+            self.bi.set_value(param_list[7].get_value())
+            self.Wxc.set_value(param_list[8].get_value())
+            self.Whc.set_value(param_list[9].get_value())
+            self.bc.set_value(param_list[10].get_value())
+            self.Wxo.set_value(param_list[11].get_value())
+            self.Who.set_value(param_list[12].get_value())
+            self.Wco.set_value(param_list[13].get_value())
+            self.bo.set_value(param_list[14].get_value())
+        else:
+            self.Whf.set_value(param_list[1].get_value())
+            self.Wcf.set_value(param_list[2].get_value())
+            self.bf.set_value(param_list[3].get_value())
+            self.Whi.set_value(param_list[4].get_value())
+            self.Wci.set_value(param_list[5].get_value())
+            self.bi.set_value(param_list[6].get_value())
+            self.Whc.set_value(param_list[7].get_value())
+            self.bc.set_value(param_list[8].get_value())
+            self.Who.set_value(param_list[9].get_value())
+            self.Wco.set_value(param_list[10].get_value())
+            self.bo.set_value(param_list[11].get_value())
+
 
--- testbed/dnn/network/stacked_networks.py
+++ testbed/dnn/network/stacked_networks.py
@@ -63,21 +63,30 @@ class StackedLSTM(StackedNetwork):
                  output=None,
                  n_ins=784,
                  hidden_layers_sizes=[500, 500],
+                 n_timesteps=None,
+                 initial_hidden_states=None,
+                 has_input=True
     ):
         self.n_ins = n_ins
         self.hidden_layers_sizes = hidden_layers_sizes
         self.n_layers = len(hidden_layers_sizes)
+        self.initial_hidden_states = initial_hidden_states
+        self.has_input = has_input
 
         # Allocate symbolic variables for the data
-        if input is None:
-            # the input minibatch data is of shape (n_timesteps, n_samples, n_ins)
-            input = T.tensor3('x', dtype=theano.config.floatX)
-        if mask is None:
-            # the input minibatch mask is of shape (n_samples, n_ins)
-            mask = T.matrix('mask', dtype=theano.config.floatX) # FIXME: not used
+        if has_input:
+            if input is None:
+                # the input minibatch data is of shape (n_timesteps, n_samples, n_ins)
+                input = T.tensor3('x', dtype=theano.config.floatX)
+            if mask is None:
+                # the input minibatch mask is of shape (n_samples, n_ins)
+                mask = T.matrix('mask', dtype=theano.config.floatX) # FIXME: not used
         if output is None:
             # the output minibatch data is of shape (n_timesteps, n_samples, n_ins)
             output = T.tensor3('y', dtype=theano.config.floatX)
+
+        self.n_timesteps  = input.shape[0] if has_input and n_timesteps is None else n_timesteps
+        self.n_samples = input.shape[1] if has_input else None
 
         super(StackedLSTM, self).__init__(numpy_rng, theano_rng, name, input, mask, output, is_rnn=True)
 
@@ -88,12 +97,15 @@ class StackedLSTM(StackedNetwork):
             # determine input size
             if i == 0:
                 input_size = self.n_ins
+                has_input = self.has_input
             else:
                 input_size = self.hidden_layers_sizes[i - 1]
+                has_input = True
 
             # build an LSTM layer
             layer = LSTM(n_in=input_size,
                          n_out=self.hidden_layers_sizes[i],
+                         has_input=has_input,
                          activation=T.tanh,
                          prefix="{0}_LSTM{1}".format(self.name,i),
                          nrng=self.numpy_rng,
@@ -103,14 +115,7 @@ class StackedLSTM(StackedNetwork):
         self.setup_scan()
 
     def setup_scan(self):
-        n_timesteps = self.x.shape[0]
-        n_samples = self.x.shape[1]
-
-        outputs_info = []
-        for layer in self.layers:
-            outputs_info += layer.outputs_info(n_samples)
-
-        # feed forward calculation
+        # forward propagation
         def step(m, x, *prev_states):
             x_ = x
             new_states = []
@@ -121,14 +126,32 @@ class StackedLSTM(StackedNetwork):
                 new_states += layer_out
             return new_states
 
+        # set sequences and function
+        if self.has_input:
+            sequences = [self.mask, self.x]
+            fn = lambda m, x, *prev_states: step(m, x, *prev_states)
+        else:
+            sequences = None
+            fn = lambda *prev_states: step(None, None, *prev_states)
+
+        # set initial hidden states
+        if self.initial_hidden_states is not None:
+            outputs_info = flatten(self.initial_hidden_states)
+        else:
+            outputs_info = []
+            for layer in self.layers:
+                outputs_info += layer.outputs_info(self.n_samples)
+
+        # scan
         rval, updates = theano.scan(
-            step,
-            sequences=[self.mask, self.x],
-            n_steps=n_timesteps,
+            fn,
+            sequences=sequences,
+            n_steps=self.n_timesteps,
             outputs_info=outputs_info,
             name="{0}_scan".format(self.name)
         )
         self.rval = rval
+        self.updates = updates
 
         # rval には n_timestamps 分の step() の戻り値 new_states が入っている
         # * rval[0]: (n_timesteps, n_samples, n_ins) の LSTM0_c
@@ -142,7 +165,7 @@ class StackedLSTM(StackedNetwork):
         '''
         :return: the output of the last layer at the last time period
         '''
-        return self.rval[-1][-1]
+        return self.outputs[-1]
 
     @property
     def outputs(self):
@@ -150,6 +173,19 @@ class StackedLSTM(StackedNetwork):
         :return: the outputs of the last layer from time period 0 to T
         '''
         return self.rval[-1]
+
+    @property
+    def last_states(self):
+        '''
+        :return The states (c, h) of all ConvLSTMs at the last time interval T. This does not include
+                the output of the encoder network, namely the output of Conv(1x1) layer
+        '''
+        return [
+            [
+                self.rval[2*i][-1],     # LSTM[i].c[T]
+                self.rval[2*i+1][-1],   # LSTM[i].h[T]
+            ] for i in xrange(self.n_layers)
+        ]
 
 
 class StackedLSTMEncoder(StackedNetwork):
@@ -392,7 +428,10 @@ class StackedConvLSTM(StackedNetwork):
             mask=None,
             output=None,
             input_shape=(1,28,28),
-            filter_shapes=[(1,1,3,3)]
+            filter_shapes=[(1,1,3,3)],
+            n_timesteps=None,
+            initial_hidden_states=None,
+            has_input=True
     ):
         '''
         Initialize StackedConvLSTM
@@ -411,19 +450,25 @@ class StackedConvLSTM(StackedNetwork):
         self.output_shape = (filter_shapes[-1][0], input_shape[1], input_shape[2])
         self.n_outs = numpy.prod(input_shape[1:])
         self.n_layers = len(filter_shapes)
+        self.initial_hidden_states = initial_hidden_states
+        self.has_input = has_input
 
         assert self.n_layers > 0
 
         # Allocate symbolic variables for the data
-        if input is None:
-            # the input minibatch data is of shape (n_timesteps, n_samples, n_feature_maps, height, width)
-            input = tensor5('x', dtype=theano.config.floatX)
-        if mask is None:
-            # the input minibatch mask is of shape (n_timesteps, n_samples, n_feature_maps)
-            mask = T.tensor3('mask', dtype=theano.config.floatX) # FIXME: not used
+        if has_input:
+            if input is None:
+                # the input minibatch data is of shape (n_timesteps, n_samples, n_feature_maps, height, width)
+                input = tensor5('x', dtype=theano.config.floatX)
+            if mask is None:
+                # the input minibatch mask is of shape (n_timesteps, n_samples, n_feature_maps)
+                mask = T.tensor3('mask', dtype=theano.config.floatX) # FIXME: not used
         if output is None:
             # the output minibatch data is of shape (n_timesteps, n_samples, n_feature_maps, height, width)
             output = tensor5('y', dtype=theano.config.floatX)
+
+        self.n_timesteps  = input.shape[0] if has_input and n_timesteps is None else n_timesteps
+        self.n_samples = input.shape[1] if has_input else None
 
         super(StackedConvLSTM, self).__init__(numpy_rng, theano_rng, name, input, mask, output, is_rnn=True)
 
@@ -433,12 +478,15 @@ class StackedConvLSTM(StackedNetwork):
             # determine input size
             if i == 0:
                 input_shape = self.input_shape
+                has_input = self.has_input
             else:
                 input_shape = self.layers[-1].output_shape
+                has_input = True
 
             # build an LSTM layer
             layer = ConvLSTM(input_shape=input_shape,
                              filter_shape=self.filter_shapes[i],
+                             has_input=has_input,
                              activation=T.tanh,
                              prefix="{0}_ConvLSTM{1}".format(self.name,i),
                              nrng=self.numpy_rng,
@@ -449,15 +497,7 @@ class StackedConvLSTM(StackedNetwork):
         self.setup_scan()
 
     def setup_scan(self):
-        n_timesteps = self.x.shape[0]
-        n_samples = self.x.shape[1]
-
-        # set initial states of layers
-        outputs_info = []
-        for layer in self.layers:
-            outputs_info += layer.outputs_info(n_samples)
-
-        # feed forward calculation
+        # forward propagation
         def step(m, x, *prev_states):
             x_ = x
             new_states = []
@@ -468,14 +508,32 @@ class StackedConvLSTM(StackedNetwork):
                 new_states += layer_out
             return new_states
 
+        # set sequences and function
+        if self.has_input:
+            sequences = [self.mask, self.x]
+            fn = lambda m, x, *prev_states: step(m, x, *prev_states)
+        else:
+            sequences = None
+            fn = lambda *prev_states: step(None, None, *prev_states)
+
+        # set initial states of layers
+        if self.initial_hidden_states is not None:
+            outputs_info = flatten(self.initial_hidden_states)
+        else:
+            outputs_info = []
+            for layer in self.layers:
+                outputs_info += layer.outputs_info(self.n_samples)
+
+        # scan
         rval, updates = theano.scan(
-            step,
-            sequences=[self.mask, self.x],
-            n_steps=n_timesteps,
+            fn,
+            sequences=sequences,
+            n_steps=self.n_timesteps,
             outputs_info=outputs_info,
             name="{0}_scan".format(self.name)
         )
         self.rval = rval
+        self.updates = updates
 
         # rval には n_timestamps 分の step() の戻り値 new_states が入っている
         # * rval[0]: (n_timesteps, n_samples, n_output_feature_maps, height, width) の LSTM0_c
@@ -489,7 +547,7 @@ class StackedConvLSTM(StackedNetwork):
         '''
         :return: the output of the last layer at the last time period
         '''
-        return self.rval[-1][-1]
+        return self.outputs[-1]
 
     @property
     def outputs(self):
@@ -498,6 +556,25 @@ class StackedConvLSTM(StackedNetwork):
         '''
         return self.rval[-1]
 
+    @property
+    def outputs_all_layers(self):
+        '''
+        :return: the outputs of all layers from time period 0 to T
+        '''
+        return T.concatenate(self.rval[1::2], axis=2)
+
+    @property
+    def last_states(self):
+        '''
+        :return The states (c, h) of all ConvLSTMs at the last time interval T. This does not include
+                the output of the encoder network, namely the output of Conv(1x1) layer
+        '''
+        return [
+            [
+                self.rval[2*i][-1],     # ConvLSTM[i].c[T]
+                self.rval[2*i+1][-1],   # ConvLSTM[i].h[T]
+            ] for i in xrange(self.n_layers)
+        ]
 
 class StackedConvLSTMEncoder(StackedNetwork):
     '''
