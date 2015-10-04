e6f0d94c3890baaa008d48c480221a8cfb552373
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2015-10-05 00:29:35 +1100

    updated experiment.py

--- testbed/experiment.py
+++ testbed/experiment.py
@@ -109,7 +109,7 @@ def exp_moving_mnist(
         patience=10,  # Number of epoch to wait before early stop if no progress
         max_epochs=5000,  # The maximum number of epoch to run
         validFreq=None,  # Compute the validation error after this number of update.
-        saveFreq=1,  # Save the parameters after every saveFreq updates
+        saveFreq=None,  # Save the parameters after every saveFreq updates
         batch_size=16,  # The batch size during training.
         valid_batch_size=64,  # The batch size used for validation/test set.
         learning_rate=1e-3,
